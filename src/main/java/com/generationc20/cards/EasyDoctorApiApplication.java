750ce914a4e917774c6cc134619b545918678e79
EliasM <elias.macip.1995@gmail.com>
2020-04-06 18:55:20 -0500

    inicio

---
+++ src/main/java/com/generationc20/cards/EasyDoctorApiApplication.java
@@ -0,0 +1,13 @@
+package com.generationc20.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDoctorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDoctorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generationc20/cards/EasyDoctorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generationc20.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDoctorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
